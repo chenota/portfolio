f84d2e9befedcdca9c97c673574d82d44e91c853
Alex Chenot <alexchenot5@gmail.com>
2025-03-18 21:41:49 -0700

    feat: Removed picture from project card

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import Profile from './components/Profile/Profile'
 import Technologies from './components/Technologies/Technologies'
 import Project from './components/Project/Project'
 
+import { FaRust, FaPython, FaJs, FaReact, FaLinux } from 'react-icons/fa6';
+import { TbBrandCpp, TbLambda } from 'react-icons/tb';
+import { BsFiletypeSql } from 'react-icons/bs';
+import { SiGnubash } from 'react-icons/si';
+import { PiMicrosoftExcelLogo } from 'react-icons/pi';
+
 function App() {
   return (
     <>
@@ -11,7 +17,16 @@ function App() {
       <div id="cards">
         <Profile work={true}/>
         <Technologies />
-        <Project title="Test Project"/>
+        <Project 
+          title="AlexC"
+          link="https://www.github.com/chenota/alexc"
+          technologies={[
+            ["Rust", <FaRust />],
+            ["Linux", <FaLinux />],
+          ]}
+          >
+            AlexC is a compiled programming language that targets x86-64 assembly.
+        </Project>
       </div>
     </>
   )
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -5,7 +5,6 @@ import TechCard from '../TechCard/TechCard';
 const defaultProps = {
     title: "Project",
     link: "https://www.github.com/chenota",
-    img: "/square.svg",
     children: "Some text about the project.",
     technologies: [
         ["Some Text", <FaGithub/>],
@@ -25,15 +24,13 @@ function Project(props) {
     // Component
     return (
         <div className="container flexCol">
-            <h1 style={{display:'flex',flexDirection:'row',justifyContent:'left',alignItems:'center',gap:20}}>
+            <h1 style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center',gap:20}}>
                 <span>{props.title}</span>
                 <a href={props.link}><FaGithub /></a>
             </h1>
             <div>
                 {props.children}
             </div>
-            <div style={{display:'flex',flexDirection:'row',justifyContent:'left',alignItems:'center',gap:20,border:'1px solid black'}}>
-                <img src={props.img} className="imgCorners"/>
                 <div style={{display:'flex',flexDirection:'row',justifyContent:'center',alignItems:'center',flexWrap:'wrap',gap:5}}>
                     {
                         props.technologies.map(([txt, ico], idx) => {
@@ -41,7 +38,6 @@ function Project(props) {
                         })
                     }
                 </div>
-            </div>
             
         </div>
     )
